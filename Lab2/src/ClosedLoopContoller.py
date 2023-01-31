c7daf7bb115fc16d0e15c76bf41df624f9708ce4
Tristan <tcavarno@gmail.com>
2023-01-31 08:39:43 -0800

    start of files stubs

---
+++ Lab2/src/ClosedLoopContoller.py
@@ -0,0 +1,34 @@
+"""!
+@file lab1.py
+This file contains a class to implement a Closed Loop P Controller
+
+TODO: Create a constructor which sets the proportional gain, initial setpoint, and other necessary parameters.
+
+TODO: Create a method run() which is called repeatedly to run the control algorithm.
+      This method should accept as parameters the setpoint and measured output.
+      It should return an actuation value which is sent to a motor in this lab but might be sent to another device in another instance, as this is a generic controller.
+
+TODO: The run() method should not contain a loop and should not print the results of its running; those things go in your main code.
+
+TODO: A method set_setpoint() to set the setpoint.
+
+TODO: A method set_Kp() to set the control gain.
+
+@author Miloh Padgett, Tristan Cavarno, Jon Abraham
+@date 30-Jan-2023 Original File
+"""
+
+class PController:
+    '''!
+
+    @param gain 
+    @param setpoint
+    '''
+    def __init__(self):
+        pass
+    def run(self):
+        pass
+    def set_setpoint(self):
+        pass
+    def set_Kp(self):
+        pass
